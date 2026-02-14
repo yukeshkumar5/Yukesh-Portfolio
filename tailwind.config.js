55935237916e124e7f08b4478726c8564ed2adbe
unknown <yukeshkumarrp17@gmail.com>
2026-02-14 11:59:52 +0530

    FirstCommit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                zinc: {
+                    900: '#18181b',
+                    950: '#09090b',
+                }
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+                mono: ['Fira Code', 'monospace']
+            }
+        }
+    }
+}
